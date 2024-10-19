b9349dd61572083c8e3c009ce9c9c8efd1023b74
Geraldoderivia <brunoroters2310@gmail.com>
2024-10-19 15:57:59 -0300

    atualização do script
    método atualizar a UI criado

--- Recuperação do jogo do Tank/Assets/Scripts/UIManager.cs
+++ Recuperação do jogo do Tank/Assets/Scripts/UIManager.cs
@@ -19,12 +19,34 @@ public class UIManager : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Inativa o botão pois o jogo acabou de começar
+        buttonIniciarPartida.gameObject.SetActive(false);
+        buttonRecomecarPartida.gameObject.SetActive(false);
+
+        //Inicia o texto como Carregando enquanto o jogo está carregando
+        textStatus.text = "Carregando...";
     }
 
-    // Update is called once per frame
-    void Update()
+    //Método responsável por atualizar a interface
+    public void AtualizarUI()
     {
-        
+        //Verifica se é o host da partida (geralmente o primeiro que entra ou cria a sala)
+        if (PhotonNetwork.IsMasterClient)
+        {
+            //Mostra o botão de iniciar partida, pois somente o host pode fazer isso
+            buttonIniciarPartida.gameObject.SetActive(true);
+
+            //Esconde de status da partida
+            textStatus.gameObject.SetActive(false);
+        }
+        else
+        {
+            //Esconde o botão de iniciar partida, pois somente o host pode fazer isso
+            buttonIniciarPartida.gameObject.SetActive(false);
+
+            //Mostra e define um texto do status da partida
+            textStatus.gameObject.SetActive(true);
+            textStatus.text = "Aguardando dono da sala iniciar a partida";
+        }
     }
 }
