4b8328b68fd994321aa58f591c08935e32c688b2
Geraldoderivia <brunoroters2310@gmail.com>
2024-10-19 16:18:48 -0300

    finalização do script

--- Recuperação do jogo do Tank/Assets/Scripts/ScoreUIController.cs
+++ Recuperação do jogo do Tank/Assets/Scripts/ScoreUIController.cs
@@ -1,8 +1,9 @@
-using System.Collections;
+using ExitGames.Client.Photon;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.UI;
+using Photon.Realtime;
 
 
 public class ScoreUIController : MonoBehaviourPunCallbacks
@@ -13,15 +14,17 @@ public class ScoreUIController : MonoBehaviourPunCallbacks
     //Número do "Actor" do Photon PUN para ter a referência de quem pertence esta UI
     public int actorNumber;
 
-    // Start is called before the first frame update
-    void Start()
+    //Método executado automaticamente pelo PhotonPun quando é identificado que algum jogador teve uma propriedade alterada
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
     {
-        
-    }
+        // Verifica se a propriedade "Score" foi alterada e se deve atualizar esta UI
+        if (changedProps.ContainsKey("Pontuacao") && targetPlayer.ActorNumber == actorNumber)
+        {
+            //Obtém a pontuação atualizada
+            int newScore = (int)changedProps["Pontuacao"];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            //Altera a UI para o jogador
+            textPontuacao.text = newScore.ToString();
+        }
     }
 }
