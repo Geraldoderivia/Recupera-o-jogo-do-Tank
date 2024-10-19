45f77d169a187359649bc59d2e7fb58f5a8fa4c2
Geraldoderivia <brunoroters2310@gmail.com>
2024-10-19 14:56:38 -0300

    criação do script tank controller criação das variáveis

---
+++ Recuperação do jogo do Tank/Assets/Scripts/TankController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class TankController : MonoBehaviourPun
+{
+    //Velocidade de rotação do tanque
+    public float _velocidadeRotacao = 100f;
+
+    //Velocidade de movimento do tanque
+    public float _velocidadeMovimento = 5f;
+
+    private Rigidbody2D rb;
+    private GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
