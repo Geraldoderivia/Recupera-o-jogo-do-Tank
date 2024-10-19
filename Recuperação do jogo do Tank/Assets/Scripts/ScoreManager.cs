65df498ad1212d85d0784eaabe478af546688354
Geraldoderivia <brunoroters2310@gmail.com>
2024-10-19 15:45:33 -0300

    atualização do código
    método adicionar a pontuação ao player

--- Recuperação do jogo do Tank/Assets/Scripts/ScoreManager.cs
+++ Recuperação do jogo do Tank/Assets/Scripts/ScoreManager.cs
@@ -1,19 +1,28 @@
-using System.Collections;
+
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
 
 public class ScoreManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    // Adiciona a pontuação ao jogador
+    public void AdicionarPontuacao(Player player)
     {
-        
-    }
+        //Inicializa a pontuação como zero e obtem a pontuação do jogador
+        int pontuacaoAtual = 0;
+        if (player.CustomProperties.ContainsKey("Pontuacao"))
+        {
+            pontuacaoAtual = (int)player.CustomProperties["Pontuacao"];
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //Adiciona a pontuação em 1
+        pontuacaoAtual += 1;
+
+        //Atualiza a pontuação no PhotonPun e notifica todos jogadores, fazendo isso, o Photon Pun executará o método OnPlayerPropertiesUpdate da classe PontuacaoUIController
+        Hashtable propriedadePontuacao = new Hashtable();
+        propriedadePontuacao["Pontuacao"] = pontuacaoAtual;
+        player.SetCustomProperties(propriedadePontuacao);
     }
 }
