d158c3de8a8b179db78c3ba56b6c389d4138f898
Geraldoderivia <brunoroters2310@gmail.com>
2024-10-19 16:06:47 -0300

    finalização do script
    método de recomeçar a partida para todos

--- Recuperação do jogo do Tank/Assets/Scripts/UIManager.cs
+++ Recuperação do jogo do Tank/Assets/Scripts/UIManager.cs
@@ -91,4 +91,24 @@ public class UIManager : MonoBehaviourPunCallbacks
             buttonRecomecarPartida.gameObject.SetActive(true);
         }
     }
+
+    [PunRPC]
+    public void RecomecarPartidaParaTodos()
+    {
+        //Esconde o texto e o botão pois a partida vai iniciar
+        textStatus.gameObject.SetActive(false);
+        buttonIniciarPartida.gameObject.SetActive(false);
+        buttonRecomecarPartida.gameObject.SetActive(false);
+
+        //Procura o objeto e classe GameManager e inicia a partida
+        var gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        gameManager.IniciarPartida();
+    }
+
+    //Método executado automaticamente pelo PhotonPun quando o jogador dono da sessão foi alterado (ex.: o criador saiu da sala)
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        //Atualiza a interface
+        AtualizarUI();
+    }
 }
