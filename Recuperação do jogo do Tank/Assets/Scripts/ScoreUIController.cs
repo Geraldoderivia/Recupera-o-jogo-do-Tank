04033927d60e2d9048cf059520786f591deb6648
Geraldoderivia <brunoroters2310@gmail.com>
2024-10-19 16:14:00 -0300

    criação de script
    criação do script score ui controller e criação das variáveis

---
+++ Recuperação do jogo do Tank/Assets/Scripts/ScoreUIController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+
+public class ScoreUIController : MonoBehaviourPunCallbacks
+{
+    //Referência da UI de texto de pontuação
+    public Text textPontuacao;
+
+    //Número do "Actor" do Photon PUN para ter a referência de quem pertence esta UI
+    public int actorNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
