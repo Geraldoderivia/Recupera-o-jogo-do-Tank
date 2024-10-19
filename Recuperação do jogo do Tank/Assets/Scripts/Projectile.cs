459f111c49762b133201a71b4ff04eb302bfcb19
Geraldoderivia <brunoroters2310@gmail.com>
2024-10-19 16:29:00 -0300

    script projectile criado e as variáveis

---
+++ Recuperação do jogo do Tank/Assets/Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class Projectile : MonoBehaviourPun
+{
+    //Velocidade da bala
+    public float velocidade = 5f;
+
+    //Tempo de vida até a bala se auto destruir
+    public float tempoDeVida = 3f;
+    float tempoDeVidaAtual = 0f;
+
+    //GameObject do tanque que disparou
+    private GameObject atirador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
