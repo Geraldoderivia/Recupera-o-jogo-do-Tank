07206c8f65fae54909096b51e3bb02f8e9dcad11
Geraldoderivia <brunoroters2310@gmail.com>
2024-10-19 22:49:41 -0300

    interfaces criadas
    interfaces idamageable e ishootable criadas

---
+++ Recuperação do jogo do Tank/Assets/Scripts/IDamageable.cs
@@ -0,0 +1,5 @@
+//Interface responsável por marcar classes que herdam dela, de serem passíveis de receber dano
+public interface IDamageable
+{
+    void ReceberDano();
+}
---
+++ Recuperação do jogo do Tank/Assets/Scripts/IShootable.cs
@@ -0,0 +1,7 @@
+
+//Interface responsável por marcar classes que herdam dela, de serem passíveis de atirar
+public interface IShootable
+{
+    void Atirar();
+}
+
