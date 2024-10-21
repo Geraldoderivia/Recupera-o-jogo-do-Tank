8488b1ba6573a02ad81a0f247e9deb9604abc1d1
Geraldoderivia <brunoroters2310@gmail.com>
2024-10-21 18:54:59 -0300

    atualização no front e script

--- Recuperação do jogo do Tank/Assets/Scripts/ScoreManager.cs
+++ Recuperação do jogo do Tank/Assets/Scripts/ScoreManager.cs
@@ -8,13 +8,13 @@ using ExitGames.Client.Photon;
 public class ScoreManager : MonoBehaviourPunCallbacks
 {
     // Adiciona a pontuação ao jogador
-    public void AdicionarPontuacao(Player player)
+    public void AdicionarPontuacao(Player atirador)
     {
         //Inicializa a pontuação como zero e obtem a pontuação do jogador
         int pontuacaoAtual = 0;
-        if (player.CustomProperties.ContainsKey("Pontuacao"))
+        if (atirador.CustomProperties.ContainsKey("Pontuacao"))
         {
-            pontuacaoAtual = (int)player.CustomProperties["Pontuacao"];
+            pontuacaoAtual = (int)atirador.CustomProperties["Pontuacao"];
         }
 
         //Adiciona a pontuação em 1
@@ -23,7 +23,7 @@ public class ScoreManager : MonoBehaviourPunCallbacks
         //Atualiza a pontuação no PhotonPun e notifica todos jogadores, fazendo isso, o Photon Pun executará o método OnPlayerPropertiesUpdate da classe PontuacaoUIController
         Hashtable propriedadePontuacao = new Hashtable();
         propriedadePontuacao["Pontuacao"] = pontuacaoAtual;
-        player.SetCustomProperties(propriedadePontuacao);
+        atirador.SetCustomProperties(propriedadePontuacao);
     }
     //Método que reseta a pontuação do player
     public void ResetarPontuacao(Player player)
